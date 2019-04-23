dbf82dba673a54ec2cca46f5779b565dee192f3a
Kevin Hill <khill@users.autosweet.com>
2019-04-23 13:44:18 -0400

    End of Build React App Video 3.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
 import './index.css';
-// import Card from './Card';
-import CardList from './CardList'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
-import {robots} from './robots';
 
 ReactDOM.render(
-<CardList robots={robots}/>
- , document.getElementById('root'));
+<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
