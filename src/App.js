f66f83b19c0e34ec654dd981c7ca6cbc2b0fedd5
Kevin Hill <khill@users.autosweet.com>
2019-04-23 15:40:10 -0400

    End of Build React App Video 4.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import {robots} from './robots';
+import './App.css'
 
 class App extends Component{
 constructor(){
   super()
   this.state={
-    robots: robots,
+    robots: [],
     searchfield:''
 }
+}
+
+componentDidMount(){
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response=> response.json())
+  .then(users=>  this.setState({robots: users}))
+
 }
 
 onSearchChange = (event) => {
@@ -19,11 +26,14 @@ render(){
   const filteredRobots = this.state.robots.filter(robots =>{
     return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
   })
+if(this.state.robots.length === 0){
+return <h1>Loading</h1>
+} else{
   return (<div className='tc'>
-    <h1>RoboFriends</h1>
+    <h1 className='f1'>RoboFriends</h1>
     <SearchBox searchChange={this.onSearchChange}/>
     <CardList robots={filteredRobots}/>
-  </div>)
+  </div>)}
 };
 }
 
