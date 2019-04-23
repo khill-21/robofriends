619dca52cd2a553434598e0e52b721e5972dc539
Kevin Hill <khill@users.autosweet.com>
2019-04-23 15:58:08 -0400

    End of Build React App Video 5.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
+import Scroll from './Scroll';
 import './App.css'
 
 class App extends Component{
@@ -32,7 +33,9 @@ return <h1>Loading</h1>
   return (<div className='tc'>
     <h1 className='f1'>RoboFriends</h1>
     <SearchBox searchChange={this.onSearchChange}/>
+<Scroll>
     <CardList robots={filteredRobots}/>
+</Scroll>
   </div>)}
 };
 }
